3eae81a01a819e221b909fc270c77bc0bd3722d7
woodley <gelinw3@gmail.com>
2023-06-19 18:14:39 -0400

    rm unecessary

--- src/Components/GiftCardComponent.js
+++ src/Components/GiftCardComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-export function GiftCardComponent({link, size}){
+export function GiftCardComponent({link, index}){
     return (
-            <img src={link} alt="GIF" width={100} height={100} style={{border: '3px solid grey'}}></img>
+        <img src={link} alt="GIF" width={100} height={100} style={{border: '3px solid grey'}}></img>
     )
 }
