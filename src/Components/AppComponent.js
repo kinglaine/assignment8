0c231af25aaaa9d9453ea6cada017f35b58aa84f
woodley <gelinw3@gmail.com>
2023-06-19 15:30:38 -0400

    add custom search feature

--- src/Components/AppComponent.js
+++ src/Components/AppComponent.js
@@ -4,38 +4,55 @@ import { useState } from "react";
 import axios from "axios";
 export function AppComponent(){
     const API_KEY = "kZ7VvaylfsjTpKaM58QZZevBfZnatCfU";
-    const[trendingGifts, setTrendingGifts] = useState([]);
-    const[currentDisplay, setCurrentDisplay] = useState("Trending Gift");
+    const[currentDisplay, setcurrentDisplay] = useState([]);
+    const[currentDisplayName, setCurrentDisplayName] = useState("Trending Gift");
+    const[searchTerm, setSearchTerm] = useState("");
     useEffect(()=>{
-        async function getTrendingGifts(){
+        async function getcurrentDisplay(){
             try {
                 await axios.get(`http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`).then((response) =>{
-                    setTrendingGifts(response.data.data);
+                    setcurrentDisplay(response.data.data);
                     console.log("aoi array",response.data.data);
                 });
             } catch (error) {
                 console.log(error);
             }
         }
-        getTrendingGifts();
+        getcurrentDisplay();
     },[]);
-   
+    //submit function to search custom GIF
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+        setCurrentDisplayName(`Search Results for "${searchTerm}"`);
+        try {
+            await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${API_KEY}`).then((response) =>{
+                setcurrentDisplay(response.data.data);
+                console.log("aoi array",response.data.data);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+    //function that monitors values in search bar and set it to setSearchTerm variable
+    const handleChange = (event) =>{
+        setSearchTerm(event.target.value);
+    }
     return (
         <div>
             <div style={{float:'right'}}>
-                <form>
-                    <input type="text" name="Search" placeholder="search GIF"></input>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="Search" placeholder="search GIF" onChange={handleChange} value={searchTerm}></input>
                     <button type="submit">Search</button>
                 </form>
             </div>
             <div>
-                <h1>{currentDisplay}</h1>
+                <h1>{currentDisplayName}</h1>
                 <div style={{textAlign: 'center', border: '5px solid black'}}>
                     {
-                        trendingGifts?.map((data)=>{
+                        currentDisplay?.map((data)=>{
                             return(
                                 //<iframe key={link.id} src={link.embed_url}/>
-                                <GiftCardComponent key={data.id} link = {data.images?.original?.url} size = {trendingGifts.length}></GiftCardComponent>
+                                <GiftCardComponent key={data.id} link = {data.images?.original?.url} size = {currentDisplay.length}></GiftCardComponent>
                             )
                         })
                     }
