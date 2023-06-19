50565db8a2c4bf03dcc4826644d87303d7b08194
woodley <gelinw3@gmail.com>
2023-06-19 15:54:01 -0400

    lift up state from search field to App component

--- src/Components/AppComponent.js
+++ src/Components/AppComponent.js
@@ -2,16 +2,16 @@ import React, { useEffect } from "react";
 import { GiftCardComponent } from "./GiftCardComponent";
 import { useState } from "react";
 import axios from "axios";
+import { SearFieldComponent } from "./SearchFieldComponent";
 export function AppComponent(){
     const API_KEY = "kZ7VvaylfsjTpKaM58QZZevBfZnatCfU";
-    const[currentDisplay, setcurrentDisplay] = useState([]);
+    const[currentDisplay, setCurrentDisplay] = useState([]);
     const[currentDisplayName, setCurrentDisplayName] = useState("Trending Gift");
-    const[searchTerm, setSearchTerm] = useState("");
     useEffect(()=>{
         async function getcurrentDisplay(){
             try {
                 await axios.get(`http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`).then((response) =>{
-                    setcurrentDisplay(response.data.data);
+                    setCurrentDisplay(response.data.data);
                     console.log("aoi array",response.data.data);
                 });
             } catch (error) {
@@ -20,31 +20,10 @@ export function AppComponent(){
         }
         getcurrentDisplay();
     },[]);
-    //submit function to search custom GIF
-    const handleSubmit = async (event) =>{
-        event.preventDefault();
-        setCurrentDisplayName(`Search Results for "${searchTerm}"`);
-        try {
-            await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${API_KEY}`).then((response) =>{
-                setcurrentDisplay(response.data.data);
-                console.log("aoi array",response.data.data);
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    } 
-    //function that monitors values in search bar and set it to setSearchTerm variable
-    const handleChange = (event) =>{
-        setSearchTerm(event.target.value);
-    }
+    
     return (
         <div>
-            <div style={{float:'right'}}>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name="Search" placeholder="search GIF" onChange={handleChange} value={searchTerm}></input>
-                    <button type="submit">Search</button>
-                </form>
-            </div>
+            <SearFieldComponent setCurrentDisplayName={setCurrentDisplayName} setcurrentDisplay={setCurrentDisplay}></SearFieldComponent>
             <div>
                 <h1>{currentDisplayName}</h1>
                 <div style={{textAlign: 'center', border: '5px solid black'}}>
